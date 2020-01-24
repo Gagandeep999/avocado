e6fafce0902aaf09e0035ffee191d559a425214f
GAGANDEEP SINGH <gagan.rishi999@gmail.com>
2020-01-24 01:19:54 -0500

    Some symbols are supported. Identifier are working, need to add support for keyword.

--- src/com/gagan_442_a1/token.java
+++ src/com/gagan_442_a1/token.java
@@ -1,18 +1,2 @@
 package com.gagan_442_a1;
 
-public class token {
-    private String token;
-    private String lexeme;
-    private int linenum;
-
-    public token(String token, String lexeme, int linenum){
-        this.token = token;
-        this.lexeme = lexeme;
-        this.linenum = linenum;
-    }
-
-    @Override
-    public String toString() {
-        return "[" + token + ", " + lexeme + ", " + linenum + "]";
-    }
-}
